2dff5a8f9246e9084f1129e9a0b2dd9f2816dc49
Boardleash <boardleash@gmail.com>
2025-07-23 18:52:34 -0400

    Replaced os.system calls with subprocess calls

--- alt-program/desktop.py
+++ alt-program/desktop.py
@@ -4,14 +4,25 @@
 ### DESCRIPTION ###
 ###################
 #
-# This is a secondary Python scriipt to hold the Desktop \
+# This is a secondary Python script to hold the Desktop \
 # setup function.  This is a test; should be imported into \
-# siren.py.
+# alt-maris.py.
 
 # Import necessary libraries
-import keyboard
-import os
 import speech_recognition
+import subprocess
+
+#################################
+### MISUNDERSTANDING FUNCTION ###
+#################################
+
+def Misunderstaning():
+    '''Tell user that speech was not recognizable.'''
+    proc1 = subprocess.Popen(["gtts-cli", "I do not understand."],
+            stdout=subprocess.PIPE)
+    proc2 = subprocess.Popen(["play", "-t", "mp3", "-"),
+            stdin=proc1.stdout, stderr=subprocess.PIPE)
+    proc2.communicate()
 
 ##############################
 ### DESKTOP SETUP FUNCTION ###
@@ -19,20 +30,20 @@ import speech_recognition
 
 def setupDesktop():
     '''Open typical applications that I use on the Desktop.'''
-      with speech_recognition.Microphone() as source:
+    with speech_recognition.Microphone() as source:
         listener = speech_recognition.Recognizer()
         audio = listener.listen(source)
         cmd_audio = listener.recognize_google(audio)
-        print("FUNCTION TEXT "+cmd_audio)
+        print("FUNCTION TEXT: "+cmd_audio)
         if cmd_audio == 'set up':
           try:
-            os.system('gtts-cli "Setting up your desktop..." | play -t mp3 -')
-            keyboard.press_and_release('ctrl+shift+n')
-            keyboard.write('brave-browser')
-            keyboard.press_and_release('enter')
-            os.system('gtts-cli "Desktop setup complete.')
+            proc1 = subprocess.Popen(["gtts-cli", "Setting up your desktop..."],
+                    stdout=subprocess.PIPE)
+            proc2 = subprocess.Popen(["play", "-t", "mp3", "-"),
+                    stdin=proc1.stdout, stderr=subprocess.PIPE)
+            proc2.communicate()
+            subprocess.Popen(["ptyxis", "-s"])
+            subprocess.call(["brave-browser"])
           except speech_recognition.UnknownValueError:
-            os.system('gtts-cli "I do not understand." | play -t mp3 -')
-        else:
-            os.system('gtts-cli "I do not understand." | play -t mp3 -')
+            Misunderstanding()
 # EOF
