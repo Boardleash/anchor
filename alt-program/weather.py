380c3fb8ac97817dcbbac0a1921475a7e5874c8e
Boardleash <boardleash@gmail.com>
2025-07-23 17:48:14 -0400

    Replaced os.system calls with subprocess calls

--- alt-program/weather.py
+++ alt-program/weather.py
@@ -5,11 +5,24 @@
 ###################
 #
 # This is a secondary Python scriipt to hold the Weather \
-# function.  This is a test; should be imported into siren.py.
+# function.  This is a test; should be imported into alt-maris.py.
 
 # Import necessary libraries
-import os
+import requests
 import speech_recognition
+import subprocess
+
+#################################
+### MISUNDERSTANDING FUNCTION ###
+#################################
+
+def Misunderstanding():
+    '''Tell user that the speech was not recognizable.'''
+    proc1 = subprocess.Popen(["gtts-cli", "I do not understand."],
+            stdout=subprocess.PIPE)
+    proc2 = subprocess.Popen(["play", "-t", "mp3", "-"],
+            stdin=proc1.stdout, stderr=subprocess.PIPE)
+    proc2.communicate()
 
 ########################
 ### WEATHER FUNCTION ###
@@ -17,29 +30,34 @@ import speech_recognition
 
 def Weather():
     '''Provide weather information to user.'''
-      with speech_recognition.Microphone() as source:
+    with speech_recognition.Microphone() as source:
         listener = speech_recognition.Recognizer()
         audio = listener.listen(source)
         cmd_audio = listener.recognize_google(audio)
-        print("FUNCTION TEXT "+cmd_audio)
+        print("FUNCTION TEXT: "+cmd_audio)
         if cmd_audio == 'current weather':
           try:
-            os.system('curl wttr.in/?format="%l+%t" > weather')
-            os.system('gtts-cli -f weather')
-            os.system('rm weather')
+            current_weather = requests.get('https://wttr.in/?format=3')
+            proc1 = subprocess.Popen(["gtts-cli", current_weather.text],
+                    stdout=subprocess.PIPE)
+            proc2 = subprocess.Popen(["play", "-t", "mp3", "-"],
+                    stdin=proc1.stdout, stderr=subprocess.PIPE)
+            proc2.communicate()
           except speech_recognition.UnknownValueError:
-            os.system('gtts-cli "I do not understand." | play -t mp3 -')
+            Misunderstanding()
         elif cmd_audio == 'weather forecast':
           try:
-            os.system('wget wttr.in/Mooresville.png')
-            os.system('xdg-open ./Mooresville.png')
-            os.system('gtts-cli "Your weather forecast for the week is on your desktop" | play -t mp3 -')
+            weather_forecast = requests.get('https://wttr.in/Mooresville.png')
+            with open("./Mooresville.png", 'wb') as forecast:
+                forecast.write(weather_forecast.content)
+                forecast.close()
+            subprocess.Popen(["xdg-open", "Mooresville.png"])
+            proc1 = subprocess.Popen(["gtts-cli", "Your weather forecast for the week is on your desktop.  The file will be removed in 30 seconds."],
+                    stdout=subprocess.PIPE)
+            proc2 = subprocess.Popen(["play", "-t", "mp3", "-"],
+                    stdin=proc1.stdout, stderr=subprocess.PIPE)
+            proc2.communicate()
           except speech_recognition.UnknownValueError:
-            os.system('gtts-cli "I do not understand." | play -t mp3 -')
-        else:
-            os.system('gtts-cli "I do not understand." | play -t mp3 -')
+            Misunderstanding()
 
 # EOF
-
-# Need to look into a different way to tell the user the current weather \
-# instead of creating a file and then removing it
