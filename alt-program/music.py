54b811fdd79d00bf9d78058e82678c70256894e2
Boardleash <boardleash@gmail.com>
2025-07-23 15:01:35 -0400

    Changed os.system calls to subprocess calls, took out some speech

--- alt-program/music.py
+++ alt-program/music.py
@@ -5,11 +5,24 @@
 ###################
 #
 # This is a secondary Python script to hold the music player \
-# function.  This is a test; should be imported into siren.py.
+# function.  This is a test; should be imported into alt-maris.py.
 
 # Import necessary libraries
 import os
 import speech_recognition
+import subprocess
+
+#################################
+### MISUNDERSTANDING FUNCTION ###
+#################################
+
+def Misunderstanding():
+    '''Tell user that the speech was not recognizable.'''
+    proc1 = subprocess.Popen(["gtts-cli", "I do not understand."],
+            stdout=subprocess.PIPE)
+    proc2 = subprocess.Popen(["play", "-t", "mp3", "-"],
+            stdin=proc1.stdout, stderr=subprocess.PIPE)
+    proc2.communicate()
 
 ######################
 ### MUSIC FUNCTION ###
@@ -21,28 +34,43 @@ def Music():
         listener = speech_recognition.Recognizer()
         audio = listener.listen(source)
         cmd_audio = listener.recognize_google(audio)
-        print("FUNCTION TEXT "+cmd_audio)
+        print("FUNCTION TEXT: "+cmd_audio)
         if cmd_audio == 'play music':
-          try:
-            os.system('gtts-cli "Playing music now..." | play -t mp3 -')
-            os.system('strawberry --quiet -p &')
-          except speech_recognition.UnknownValueError:
-            os.system('gtts-cli "I do not understand." | play -t mp3 -')
+            try:
+                subprocess.Popen(["strawberry", "--quiet", "-p"])
+                return quit() 
+            except speech_recognition.UnknownValueError:
+                Misunderstanding()
         elif cmd_audio == 'stop music':
-          try:
-            os.system('gtts-cli "Stopping music now..." | play -t mp3 -')
-            os.system('strawberry --quiet -s &')
-          except speech_recognition.UnknownValueError:
-            os.system('gtts-cli "I do not understand." | play -t mp3 -')
+            try:
+                subprocess.Popen(["strawberry", "--quiet", "-s"])
+                return quit() 
+            except speech_recognition.UnknownValueError:
+                Misunderstanding()
         elif cmd_audio == 'volume up':
-          try:
-            os.system('strawberry --quiet --volume-up &')
-          except speech_recognition.UnknownValueError:
-            os.system('gtts-cli "I do not understand." | play -t mp3 -')
+            try:
+                subprocess.Popen(["strawberry", "--quiet", "--volume-up"])
+                return quit() 
+            except speech_recognition.UnknownValueError:
+                Misunderstanding()
         elif cmd_audio == 'volume down':
-          try:
-            os.system('strawberry --quiet --volume-down &')
-          except speech_recognition.UnknownValueError:
-            os.system('gtts-cli "I do not understand." | play -t mp3 -')
+            try:
+                subprocess.Popen(["strawberry", "--quiet", "--volume-down"])
+                return quit() 
+            except speech_recognition.UnknownValueError:
+                Misunderstanding()
+        elif cmd_audio == 'next':
+            try:
+                subprocess.Popen(["strawberry", "--quiet", "-f"])
+                return quit()
+            except speech_recognition.UnknownValueError:
+                Misunderstanding()
+        elif cmd_audio == 'close music':
+            try:
+                proc1 = subprocess.check_output(["pgrep", "--full", "strawberry --quiet -p"], encoding='utf-8').strip()
+                proc2 = subprocess.Popen(["kill", "-s", "SIGTERM", proc1])
+                return quit()
+            except speech_recognition.UnknownValueError:
+                Misunderstanding()
 
 # EOF
