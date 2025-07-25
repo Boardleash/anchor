0c58481f438d6c55ed03e45cdbf7ed0ead150279
Boardleash <boardleash@gmail.com>
2025-07-25 11:54:43 -0400

    Fixed indentation and syntax issues

--- alt-program/desktop.py
+++ alt-program/desktop.py
@@ -20,7 +20,7 @@ def Misunderstaning():
     '''Tell user that speech was not recognizable.'''
     proc1 = subprocess.Popen(["gtts-cli", "I do not understand."],
             stdout=subprocess.PIPE)
-    proc2 = subprocess.Popen(["play", "-t", "mp3", "-"),
+    proc2 = subprocess.Popen(["play", "-t", "mp3", "-"],
             stdin=proc1.stdout, stderr=subprocess.PIPE)
     proc2.communicate()
 
@@ -35,15 +35,16 @@ def setupDesktop():
         audio = listener.listen(source)
         cmd_audio = listener.recognize_google(audio)
         print("FUNCTION TEXT: "+cmd_audio)
-        if cmd_audio == 'set up':
-          try:
-            proc1 = subprocess.Popen(["gtts-cli", "Setting up your desktop..."],
-                    stdout=subprocess.PIPE)
-            proc2 = subprocess.Popen(["play", "-t", "mp3", "-"),
-                    stdin=proc1.stdout, stderr=subprocess.PIPE)
-            proc2.communicate()
-            subprocess.Popen(["ptyxis", "-s"])
-            subprocess.call(["brave-browser"])
-          except speech_recognition.UnknownValueError:
-            Misunderstanding()
+        if cmd_audio == "desktop":
+            try:
+                proc1 = subprocess.Popen(["gtts-cli", "Setting up your desktop..."],
+                        stdout=subprocess.PIPE)
+                proc2 = subprocess.Popen(["play", "-t", "mp3", "-"],
+                        stdin=proc1.stdout, stderr=subprocess.PIPE)
+                proc2.communicate()
+                subprocess.Popen(["ptyxis", "-s"])
+                subprocess.call(["brave-browser"])
+                return quit()
+            except speech_recognition.UnknownValueError:
+                Misunderstanding()
 # EOF
