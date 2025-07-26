8734739030f100bba008ba4bd20f0599c68a856d
Boardleash <boardleash@gmail.com>
2025-07-26 12:43:13 -0400

    Changed from gtts-cli to pyttsx3; changed google recognition to vosk

--- alt-program/music.py
+++ alt-program/music.py
@@ -5,23 +5,27 @@
 ###################
 #
 # This is a secondary Python script to hold the music player \
-# function.  This is a test; should be imported into alt-maris.py.
+# function.  This is a test; should be imported into alt-anchor.py.
 
 # Import necessary libraries
+import pyttsx3
+import re
 import speech_recognition
 import subprocess
+import vosk
 
-#################################
-### MISUNDERSTANDING FUNCTION ###
-#################################
+# Initialize SpeechRecognition and PyTTSx3.
+listener = speech_recognition.Recognizer()
+speaker = pyttsx3.init()
 
-def Misunderstanding():
-    '''Tell user that the speech was not recognizable.'''
-    proc1 = subprocess.Popen(["gtts-cli", "I do not understand."],
-            stdout=subprocess.PIPE)
-    proc2 = subprocess.Popen(["play", "-t", "mp3", "-"],
-            stdin=proc1.stdout, stderr=subprocess.PIPE)
-    proc2.communicate()
+########################
+### PYTTSX3 SETTINGS ###
+########################
+
+#speech_rate = speaker.setProperty('rate', 150)
+#speech_volume = speaker.setProperty('volume', 0.50)
+#speech_language = speaker.getProperty('voices')
+#speech_voice = speaker.setProperty('voice', speech_language[23].id)
 
 ######################
 ### MUSIC FUNCTION ###
@@ -30,46 +34,51 @@ def Misunderstanding():
 def Music():
     '''Control the onboard music player.'''
     with speech_recognition.Microphone() as source:
-        listener = speech_recognition.Recognizer()
         audio = listener.listen(source)
-        cmd_audio = listener.recognize_google(audio)
-        print("FUNCTION TEXT: "+cmd_audio)
-        if cmd_audio == 'play music':
+        rcvd_audio = listener.recognize_vosk(audio)
+        cmd_list = ["play music", "stop music", "volume up", "volume down", "next", "close music"]
+        if re.findall(cmd_list[0], rcvd_audio):
             try:
                 subprocess.Popen(["strawberry", "--quiet", "-p"])
                 return quit() 
             except speech_recognition.UnknownValueError:
-                Misunderstanding()
-        elif cmd_audio == 'stop music':
+                speaker.say("I do not understand")
+                speaker.runAndWait()
+        elif re.findall(cmd_list[1], rcvd_audio):
             try:
                 subprocess.Popen(["strawberry", "--quiet", "-s"])
                 return quit() 
             except speech_recognition.UnknownValueError:
-                Misunderstanding()
-        elif cmd_audio == 'volume up':
+                speaker.say("I do not understand")
+                speaker.runAndWait()
+        elif re.findall(cmd_list[2], rcvd_audio):
             try:
                 subprocess.Popen(["strawberry", "--quiet", "--volume-up"])
                 return quit() 
             except speech_recognition.UnknownValueError:
-                Misunderstanding()
-        elif cmd_audio == 'volume down':
+                speaker.say("I do not understand")
+                speaker.runAndWait()
+        elif re.findall(cmd_list[3], rcvd_audio):
             try:
                 subprocess.Popen(["strawberry", "--quiet", "--volume-down"])
                 return quit() 
             except speech_recognition.UnknownValueError:
-                Misunderstanding()
-        elif cmd_audio == 'next':
+                speaker.say("I do not understand")
+                speaker.runAndWait()
+        elif re.findall(cmd_list[4], rcvd_audio):
             try:
                 subprocess.Popen(["strawberry", "--quiet", "-f"])
                 return quit()
             except speech_recognition.UnknownValueError:
-                Misunderstanding()
-        elif cmd_audio == 'close music':
+                speaker.say("I do not understand.")
+                speaker.runAndWait()
+        elif re.findall(cmd_list[5], rcvd_audio):
             try:
                 proc1 = subprocess.check_output(["pgrep", "--full", "strawberry --quiet -p"], encoding='utf-8').strip()
                 proc2 = subprocess.Popen(["kill", "-s", "SIGTERM", proc1])
                 return quit()
             except speech_recognition.UnknownValueError:
-                Misunderstanding()
+                speaker.say("I do not understand.")
+                speaker.runAndWait()
 
 # EOF
