52731a815e0608e9937f0ae2050b33d480ae2fef
Boardleash <boardleash@gmail.com>
2025-07-26 12:44:58 -0400

    Changed gtts-cli to pyttsx3; changed google recognition to vosk

--- alt-program/weather.py
+++ alt-program/weather.py
@@ -5,24 +5,29 @@
 ###################
 #
 # This is a secondary Python scriipt to hold the Weather \
-# function.  This is a test; should be imported into alt-maris.py.
+# function.  This is a test; should be imported into alt-anchor.py.
 
 # Import necessary libraries
+import pyttsx3
+import re
 import requests
 import speech_recognition
 import subprocess
+import time
+import vosk
 
-#################################
-### MISUNDERSTANDING FUNCTION ###
-#################################
+# Initialize SpeechRecognition and PyTTSx3.
+listener = speech_recognition.Recognizer()
+speaker = pyttsx3.init()
 
-def Misunderstanding():
-    '''Tell user that the speech was not recognizable.'''
-    proc1 = subprocess.Popen(["gtts-cli", "I do not understand."],
-            stdout=subprocess.PIPE)
-    proc2 = subprocess.Popen(["play", "-t", "mp3", "-"],
-            stdin=proc1.stdout, stderr=subprocess.PIPE)
-    proc2.communicate()
+########################
+### PYTTSX3 SETTINGS ###
+########################
+
+#speech_rate = speaker.setProperty('rate', 150)
+#speech_volume = speaker.setProperty('volume', 0.50)
+#speech_language = speaker.getProperty('voices')
+#speech_voice = speaker.setProperty('voice', speech_language[23].id)
 
 ########################
 ### WEATHER FUNCTION ###
@@ -31,35 +36,31 @@ def Misunderstanding():
 def Weather():
     '''Provide weather information to user.'''
     with speech_recognition.Microphone() as source:
-        listener = speech_recognition.Recognizer()
         audio = listener.listen(source)
-        cmd_audio = listener.recognize_google(audio)
-        print("FUNCTION TEXT: "+cmd_audio)
-        if cmd_audio == 'current weather':
+        rcvd_audio = listener.recognize_vosk(audio)
+        if re.findall("current weather", rcvd_audio):
           try:
             current_weather = requests.get('https://wttr.in/?format=3')
-            proc1 = subprocess.Popen(["gtts-cli", current_weather.text],
-                    stdout=subprocess.PIPE)
-            proc2 = subprocess.Popen(["play", "-t", "mp3", "-"],
-                    stdin=proc1.stdout, stderr=subprocess.PIPE)
-            proc2.communicate()
+            speaker.say(current_weather.text)
+            speaker.runAndWait()
             return quit()
           except speech_recognition.UnknownValueError:
-            Misunderstanding()
-        elif cmd_audio == 'weather forecast':
+            speaker.say("I do not understand.")
+            speaker.runAndWait()
+        elif re.findall("weather forecast", rcvd_audio):
           try:
             weather_forecast = requests.get('https://wttr.in/Mooresville.png')
             with open("./Mooresville.png", 'wb') as forecast:
                 forecast.write(weather_forecast.content)
                 forecast.close()
             subprocess.Popen(["xdg-open", "Mooresville.png"])
-            proc1 = subprocess.Popen(["gtts-cli", "Your weather forecast for the week is on your desktop.  The file will be removed in 30 seconds."],
-                    stdout=subprocess.PIPE)
-            proc2 = subprocess.Popen(["play", "-t", "mp3", "-"],
-                    stdin=proc1.stdout, stderr=subprocess.PIPE)
-            proc2.communicate()
+            speaker.say("Your weather forecast for the week is on your desktop.  The file will be removed in 30 seconds.")
+            speaker.runAndWait()
+            time.sleep(30)
+            subprocess.Popen(["rm", "Mooresville.png"])
             return quit()
           except speech_recognition.UnknownValueError:
-            Misunderstanding()
+            speaker.say("I do not understand.")
+            speaker.runAndWait()
 
 # EOF
