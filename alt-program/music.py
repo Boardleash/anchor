1946659954a35e83ab61d8220279cab008568f34
Boardleash <boardleash@gmail.com>
2025-07-28 12:17:07 -0400

    Added Windows Media Player condition

--- alt-program/music.py
+++ alt-program/music.py
@@ -12,6 +12,7 @@ import pyttsx3
 import re
 import speech_recognition
 import subprocess
+import sys
 import vosk
 
 # Initialize SpeechRecognition and PyTTSx3.
@@ -37,47 +38,53 @@ def Music():
         audio = listener.listen(source)
         rcvd_audio = listener.recognize_vosk(audio)
         if re.findall("play music", rcvd_audio):
-            try:
-                subprocess.Popen(["strawberry", "--quiet", "-p"])
-                return quit() 
-            except speech_recognition.UnknownValueError:
-                speaker.say("I do not understand")
-                speaker.runAndWait()
-        elif re.findall("stop music", rcvd_audio):
-            try:
-                subprocess.Popen(["strawberry", "--quiet", "-s"])
-                return quit() 
-            except speech_recognition.UnknownValueError:
-                speaker.say("I do not understand")
-                speaker.runAndWait()
-        elif re.findall("volume up", rcvd_audio):
-            try:
-                subprocess.Popen(["strawberry", "--quiet", "--volume-up"])
-                return quit() 
-            except speech_recognition.UnknownValueError:
-                speaker.say("I do not understand")
-                speaker.runAndWait()
-        elif re.findall("volume down", rcvd_audio):
-            try:
-                subprocess.Popen(["strawberry", "--quiet", "--volume-down"])
-                return quit() 
-            except speech_recognition.UnknownValueError:
-                speaker.say("I do not understand")
-                speaker.runAndWait()
-        elif re.findall("next", rcvd_audio):
-            try:
-                subprocess.Popen(["strawberry", "--quiet", "-f"])
-                return quit()
-            except speech_recognition.UnknownValueError:
-                speaker.say("I do not understand.")
-                speaker.runAndWait()
-        elif re.findall("close music", rcvd_audio):
-            try:
-                proc1 = subprocess.check_output(["pgrep", "--full", "strawberry --quiet -p"], encoding='utf-8').strip()
-                proc2 = subprocess.Popen(["kill", "-s", "SIGTERM", proc1])
-                return quit()
-            except speech_recognition.UnknownValueError:
-                speaker.say("I do not understand.")
-                speaker.runAndWait()
-
+            if sys.platform == "linux":
+                try:
+                    subprocess.Popen(["strawberry", "--quiet", "-p"])
+                    return quit() 
+                except speech_recognition.UnknownValueError:
+                    speaker.say("I don't understand.")
+                    speaker.runAndWait()
+            elif re.findall("stop music", rcvd_audio):
+                try:
+                    subprocess.Popen(["strawberry", "--quiet", "-s"])
+                    return quit() 
+                except speech_recognition.UnknownValueError:
+                    speaker.say("I don't understand.")
+                    speaker.runAndWait()
+            elif re.findall("volume up", rcvd_audio):
+                try:
+                    subprocess.Popen(["strawberry", "--quiet", "--volume-up"])
+                    return quit() 
+                except speech_recognition.UnknownValueError:
+                    speaker.say("I don't understand.")
+                    speaker.runAndWait()
+            elif re.findall("volume down", rcvd_audio):
+                try:
+                    subprocess.Popen(["strawberry", "--quiet", "--volume-down"])
+                    return quit() 
+                except speech_recognition.UnknownValueError:
+                    speaker.say("I don't understand.")
+                    speaker.runAndWait()
+            elif re.findall("next", rcvd_audio):
+                try:
+                    subprocess.Popen(["strawberry", "--quiet", "-f"])
+                    return quit()
+                except speech_recognition.UnknownValueError:
+                    speaker.say("I don't understand.")
+                    speaker.runAndWait()
+            elif re.findall("close music", rcvd_audio):
+                try:
+                    proc1 = subprocess.check_output(["pgrep", "--full", "strawberry --quiet -p"], encoding='utf-8').strip()
+                    subprocess.Popen(["kill", "-s", "SIGTERM", proc1])
+                    return quit()
+                except speech_recognition.UnknownValueError:
+                    speaker.say("I don't understand.")
+                    speaker.runAndWait()
+        elif sys.platform == "win32":
+                try:
+                    subprocess.Popen(["powershell.exe", "start-process", "MediaPlayer.exe"])
+                except speech_recognition.UnknownValueError:
+                    speaker.say("I don't understand.")
+                    speaker.runAndWait()
 # EOF
