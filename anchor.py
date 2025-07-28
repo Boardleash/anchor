458d6bea1379a1638c3501dd8df94d6c5ea685b8
Boardleash <boardleash@gmail.com>
2025-07-28 17:57:31 -0400

    Added functions to main script for execution.  TEST SAT LINUX

--- anchor.py
+++ anchor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 ##################################
-### TITLE: maris.py            ###
+### TITLE: anchor.py           ###
 ### AUTHOR: Boardleash (Derek) ###
 ### DATE: Friday, July 18 2025 ###
 ##################################
@@ -10,184 +10,216 @@
 ### DESCRIPTION ###
 ###################
 #
-# This is a speech program in Python that will \
-# take certain voice commands and run appropriate actions \
-# based on those commands.
-# Required installs:
-#   --> pip install keyboard
-#   --> pip install pyaudio
-#   --> pip install pyttsx3 
-#   --> pip install SpeechRecognition
-#   --> "play" on a Linux system
-#   --> pip install gtts-cli (to be used with os.system) Linux
+# This is a speech program in Python that will take certain \
+# voice commands and run appropriate actions based on those commands.
 
+# Required installs:
+#   --> pip install pyttsx3
+#   --> pip install sounddevice
+#   --> pip install SpeechRecognition
+#   --> pip install vosk
 
 # Import needed libraries.
-import keyboard
-import os
+import json
 import pyttsx3
+import re
+import requests
+import sounddevice
 import speech_recognition
+import subprocess
+import sys
+import time
+import vosk
+from vosk import SetLogLevel
 
 # Initialize SpeechRecognition and PyTTSx3.
 listener = speech_recognition.Recognizer()
-#speaker = pyttsx3.init()
+speaker = pyttsx3.init()
 
 ########################
 ### PYTTSX3 SETTINGS ###
 ########################
 
-#speech_rate = speaker.setProperty('rate', 115)
-#speech_volume = speaker.setProperty('volume', 0.75)
-#speech_language = speaker.getProperty('voices')
-#speech_voice = speaker.setProperty('voice', speech_language[24].id)
-#speaker.say("Good afternoon Derek!")
-#speaker.runAndWait()
+speech_rate = speaker.setProperty('rate', 125)
+speech_volume = speaker.setProperty('volume', 0.75)
+speech_language = speaker.getProperty('voices')
+speech_voice = speaker.setProperty('voice', speech_language[23].id)
 
-###################################
-### SPEECH RECOGNITION SETTINGS ###
-###################################
+########################
+### AGENDA FUNCTIONS ###
+########################
 
-# Speech Recognition Dynamic Ambient Noise Setting (True or False)
-#listener.dynamic_energy_threshold = True
-# Speech Recognition Dynamic Ambient Noise Adjustment (Shouldn't need to modify)
-#listener.dynamic_energy_adjustment_damping = 0.15
-# Speech Recognition Minimum Length of Silence (seconds) \
-# Smaller values = faster recognition, but will cut off slower speech
-#listener.pause_threshold = 0.8
-# Speech Recognition API Methods
-# listener.recognize_google
-# listener.recognize_google_cloud
-# listener.recognize_wit
-# listener.recognize_bing
-# listener.recognize_houndify
-# listener.recognize_ibm
-# listener.recognize_vosk (works offline)
-# listener.recognize_whisper (works offline)
-# listener.recognize_faster_whisper
-# listener.recognize_openai
-# listener.recognize_groq
+def createAgenda():
+    '''Create an agenda file with a task list.'''
+    speaker.say("Ok, what do you need to get done?")
+    speaker.runAndWait()
+    if sys.platform == "linux":
+        time.sleep(3)
+    else:
+        pass
+    listening = True
+    while listening:
+        with speech_recognition.Microphone() as source:
+            agenda_audio = listener.listen(source)
+            task_audio = listener.recognize_vosk(agenda_audio)
+            audio_to_parse = json.loads(task_audio)
+            parsed_audio = audio_to_parse["text"]
+            if re.findall("complete", task_audio):
+                temp_file.close()
+                listening = False
+                removal = "complete"
+                with open('temp.tmp', 'r') as temp_in, open('agenda', 'w') as agenda_out:
+                    tasks = temp_in.readlines()
+                    for task in tasks:
+                        scrubbed_tasks = task.replace(removal, "")
+                        agenda_out.write(scrubbed_tasks)
+                temp_in.close()
+                agenda_out.close()
+                if sys.platform == "linux":
+                    subprocess.Popen(["rm", "./temp.tmp"])
+                elif sys.platform == "win32":
+                    subprocess.Popen(["powershell.exe", "rm", "./temp.tmp"])
+                speaker.say("Your agenda has been created.")
+                speaker.runAndWait()
+                return quit()
+            else:
+                with open('temp.tmp', 'a') as temp_file:
+                    temp_file.write(parsed_audio+'\n')
+
+def readAgenda():
+    '''Read the agenda that a user created.'''
+    with open('agenda', 'r') as agenda:
+        tasks = agenda.readlines()
+        speaker.say(tasks)
+        speaker.runAndWait()
+        agenda.close()
+    return quit()
+
+##############################
+### DESKTOP SETUP FUNCTION ###
+##############################
+
+def Desktop():
+    '''Open typical applications that I use on the Desktop.'''
+    speaker.say("Setting up your desktop...")
+    speaker.runAndWait()
+    if sys.platform == "linux":
+        subprocess.Popen(["ptyxis", "-s"])
+        subprocess.call(["brave-browser"])
+    elif sys.platform == "win32":
+        subprocess.Popen(["powershell.exe", "-command", "code"])
+        brave_path = Path('"C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"')
+        subprocess.Popen(["powershell.exe","start-process", brave_path])
+    return quit()
 
 ##############################
 ### MUSIC PLAYER FUNCTIONS ###
 ##############################
 
 def playMusic():
-    '''Opens the strawberry music player and starts playing music.'''
-    os.system('gtts-cli "playing music now..." | play -t mp3 -')
-    os.system('strawberry --quiet -p &')
+    '''Open the native music player.'''
+    if sys.platform == "linux":
+        subprocess.Popen(["strawberry", "--quiet", "-p"])
+        return quit() 
+    elif sys.platform == "win32":
+        subprocess.Popen(["powershell.exe", "start-process", "MediaPlayer.exe"])
 
 def stopMusic():
-    '''Stops music from playing on strawberry music player.'''
-    os.system('gtts-cli "stopping music now..." | play -t mp3 -')
-    os.system('strawberry --quiet -s &')
+    '''Stop any music that may be playing.'''
+    subprocess.Popen(["strawberry", "--quiet", "-s"])
+    return quit() 
 
-def volumeUp():
-    '''Turn volume up on the strawberry music player.'''
-    os.system('strawberry --quiet --volume-up &')
+def upVolume():
+    '''Adjust music volume in an upward direction.'''
+    subprocess.Popen(["strawberry", "--quiet", "--volume-up"])
+    return quit() 
 
-def volumeDown():
-    '''Turn volume down on the strawberry music player.'''
-    os.system('strawberry --quiet --volume-down &')
+def downVolume():
+    '''Adjust music volume in a downward direction.'''
+    subprocess.Popen(["strawberry", "--quiet", "--volume-down"])
+    return quit() 
+
+def nextSong():
+    '''Skip to next song.'''
+    subprocess.Popen(["strawberry", "--quiet", "-f"])
+    return quit()
+
+def closeMusic():
+    '''Close the music player.'''
+    proc1 = subprocess.check_output(["pgrep", "--full", "strawberry --quiet -p"], encoding='utf-8').strip()
+    subprocess.Popen(["kill", "-s", "SIGTERM", proc1])
+    return quit()
 
 #########################
 ### WEATHER FUNCTIONS ###
 #########################
 
 def currentWeather():
-    '''Get basic current weather information.'''
-    ### NEED TO STILL WORK ON THIS ###
-    os.system('curl wttr.in/?format="%l+%t" > weather')
-    os.system('gtts-cli -f weather')
-    os.system('rm weather')
+    '''Provide current weather information to user.'''
+    current_weather = requests.get('https://wttr.in/?format=3')
+    speaker.say(current_weather.text)
+    speaker.runAndWait()
+    return quit()
 
 def forecastWeather():
-    '''Get the weather forecast in a PNG image and have it open up for presentation.'''
-    os.system('wget wttr.in/Mooresville.png')
-    os.system('xdg-open ./Mooresville.png')
-    os.system('gtts-cli "Your weather forecast for the week is on your desktop" | play -t mp3 -')
+    '''Provide 3 day weather forecast displayed on desktop.'''
+    weather_forecast = requests.get('https://wttr.in/Mooresville.png')
+    with open("Mooresville.png", 'wb') as forecast:
+        forecast.write(weather_forecast.content)
+        forecast.close()
+    if sys.platform == "linux":
+      subprocess.Popen(["xdg-open", "Mooresville.png"])
+    elif sys.platform == "win32":
+      forecast_image = Image.open('Mooresville.png')
+      forecast_image.show()
+    speaker.say("Your weather forecast for the next three days is on your desktop.")
+    speaker.runAndWait()
+    return quit()
 
-##############################
-### DESKTOP SETUP FUNCTION ###
-##############################
+#####################
+### MAIN FUNCTION ###
+#####################
 
-### NEEDS WORKED ON ###
-def setupDesktop():
-    '''Open typical applications that I use'''
-    os.system('gtts-cli "Setting up your desktop..." | play -t mp3 -')
-    keyboard.press_and_release('ctrl+shift+n')
-    keyboard.write('brave-browser')
-    keyboard.press_and_release('enter')
+def Main():
+    # Suppress log output from VOSK.
+    SetLogLevel(-1)
 
-#######################
-### AGENDA FUNCTION ###
-#######################
+    # Set condition for while loop.
+    listening = True
 
-def agenda():
-    '''Tell the user the list of 'todos' today from the agenda file.'''
-    os.system('gtts-cli -f agenda | play -t mp3 -')
-
-####################
-### MAIN PROGRAM ###
-####################
-
-def misunderstanding():
-    '''Tell user that there is a misunderstanding'''
-    os.system('gtts-cli "I do not understand." | play -t mp3 -')
-
-listening = True
-while listening:
-    # Configure a microphone to be used as a source.
-    with speech_recognition.Microphone() as source:
-        # At this point, speak into the microphone.
-        # Store what is spoken into a variable.
-        audio = listener.listen(source)
-        # Use Google to transcribe the received audio.
-        rcvd_audio = listener.recognize_google(audio)
-        # The below print call is a check to verify it got the right message.
-        print("Text: "+listener.recognize_google(audio))
-        # Conditional statements based on key phrases.
-        if rcvd_audio == 'Maris play music':
-            try:
-                playMusic() 
-            except:
-                misunderstanding()
-        elif rcvd_audio == 'Maris stop music':
-            try:
+    while listening:
+        with speech_recognition.Microphone() as source:
+            audio = listener.listen(source)
+            rcvd_audio = listener.recognize_vosk(audio)
+            print(rcvd_audio)
+            if re.findall("anchor agenda create", rcvd_audio):
+                createAgenda()
+            elif re.findall("anchor agenda read", rcvd_audio):
+                readAgenda()
+            elif re.findall("anchor desktop", rcvd_audio):
+                Desktop()
+            elif re.findall("anchor music play", rcvd_audio):
+                playMusic()
+            elif re.findall("anchor music stop", rcvd_audio):
                 stopMusic()
-            except:
-                misunderstanding()
-        elif rcvd_audio == 'Maris volume up':
-            try:
-                volumeUp()
-            except:
-                misunderstanding()
-        elif rcvd_audio == 'Maris volume down':
-            try:
-                volumeDown()
-            except:
-                misunderstanding()
-        elif rcvd_audio == 'Maris current weather':
-            try:
+            elif re.findall("anchor volume up", rcvd_audio):
+                upVolume()
+            elif re.findall("anchor volume down", rcvd_audio):
+                downVolume()
+            elif re.findall("anchor next", rcvd_audio):
+                nextSong()
+            elif re.findall("anchor close", rcvd_audio):
+                closeMusic()
+            elif re.findall("anchor current weather", rcvd_audio):
                 currentWeather()
-            except:
-                misunderstanding()
-        elif rcvd_audio == 'Maris forecast':
-            try:
+            elif re.findall("anchor weather forecast", rcvd_audio):
                 forecastWeather()
-            except:
-                misunderstanding()
-        elif rcvd_audio == 'Maris set up':
-            try:
-                setupDesktop()
-            except:
-                misunderstanding()
-        elif rcvd_audio == 'Maris agenda':
-            try:
-                agenda()
-            except:
-                misunderstanding()
-        else:
-            misunderstanding()
-            listening = False
+            else:
+                speaker.say("I apologize, but I don't understand.")
+                speaker.runAndWait()
+                listening = False
+
+# Execute the script.
+Main()
 
 # EOF
